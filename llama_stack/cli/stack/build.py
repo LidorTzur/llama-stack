29b8ed172770471fee37a411dcbfa2a53be1cbc7
Yuan Tang <terrytangyuan@gmail.com>
2024-12-21 12:06:17 -0500

    Fix lint
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- llama_stack/cli/stack/build.py
+++ llama_stack/cli/stack/build.py
@@ -7,6 +7,7 @@
 import argparse
 
 import importlib.resources
+
 from llama_stack.cli.subcommand import Subcommand
 from llama_stack.distribution.datatypes import *  # noqa: F403
 import os
--- llama_stack/distribution/build.py
+++ llama_stack/distribution/build.py
@@ -4,11 +4,11 @@
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
 
+import importlib.resources
 import logging
 from enum import Enum
 from typing import List
 
-import importlib.resources
 from pydantic import BaseModel
 from termcolor import cprint
 
